10006a1992470f9cea4bd214488a9791a0176c4b
Stuart Forrester <stforrester@gmail.com>
2024-11-03 23:51:09 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(destinationBlock) {
+    return Math.abs(destinationBlock - 42);
+}
+
+function distanceFromHqInFeet(destinationBlock) {
+    return distanceFromHqInBlocks(destinationBlock) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, destinationBlock) {
+    return Math.abs(destinationBlock - startingBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    }
+    else if ((distanceTravelledInFeet(start, destination) > 400) && (distanceTravelledInFeet(start, destination) <= 2000)) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    }
+    else if ((distanceTravelledInFeet(start, destination) > 2000) && (distanceTravelledInFeet(start, destination) <= 2500)) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
